246e7e289d34a5a878d51868002744a2cc087b61
Avraz  Zebary <avrazebary@gmail.com>
2021-05-16 14:27:00 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -3,3 +3,25 @@ const testVar = {}
 function testFunc() {
   return "hi"
 }
+
+
+const record = [
+  {year: "2018", result: "L"},
+  {year: "2017", result: "W"},
+  {year: "2016", result: "N/A"}
+  //...
+]
+
+function superbowlWin(){
+  
+  
+  
+  record.find(s=> s=== "W"))
+    record.filter(s => "W");
+  return record.year;
+ 
+ 
+
+  
+
+}
